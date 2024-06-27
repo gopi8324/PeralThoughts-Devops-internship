f517d00bc304b91c130572d41054fa6cbf564f7d
gopi8324 <148228946+gopi8324@users.noreply.github.com>
2024-06-27 12:00:03 +0530

    Create install.sh

---
+++ terraform/install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install npm -y
+sudo apt install postgresql postgresql-contrib
+sudo systemctl start postgresql.service
+curl -fsSL https://get.docker.com -o install-docker.sh
+sh install-docker.sh --dry-run
+sudo sh install-docker.sh
+sudo usermod -aG docker ubuntu

5c0e737b07391fb08e7850555825b461ba7eee47
gopi <nallagorlagopi3695@gmail.com>
2024-07-06 14:06:12 +0530

    added changes in terraform file

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "aws" {
   region = "us-west-1"
+  access_key = "AKIAXYKJQKV6SQEU6EV3"
+  secret_key = "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"
   default_tags {
     tags = {
       CreatedBy   = "gopi"

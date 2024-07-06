9f497c2a1ea0024c7746fa053f86977c0937a4d2
gopi <148228946+gopi8324@users.noreply.github.com>
2024-07-06 13:58:24 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "aws" {
   region = "us-west-1"
+AWS_ACCESS_KEY_ID: ${{ secrets.ACCESS_KEY}}
+AWS_SECRET_ACCESS_KEY: ${{ secrets.SECRET.ACCESS_KEY}}
   default_tags {
     tags = {
       CreatedBy   = "gopi"

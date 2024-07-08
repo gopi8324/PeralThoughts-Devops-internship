e27cf57ba4a73a8e02f6c3753da79fc007587a8b
gopi <148228946+gopi8324@users.noreply.github.com>
2024-07-08 12:43:24 +0530

    added a node js script

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 sudo apt update
-sudo apt install npm -y
+sudo apt install unzip -y
+# command to install node on linux machine and node version is v20.15.0
+curl -fsSL https://fnm.vercel.app/install | bash
+source /home/ubuntu/.bashrc
+fnm use --install-if-missing 20
+# command to install npm
+curl -qL https://www.npmjs.com/install.sh | sh
 npm -version
 # making a directory
 mkdir /srv/strapi
@@ -8,5 +14,3 @@ mkdir /srv/strapi
 cd /srv/strapi
 # adding file into that directory
 git clone https://github.com/gopi8324/PeralThoughts-Devops-internship.git
-
-git branch
